ab3f29da9880640d06054eb369168f87dd604cf5
GreenEmerald-Branding <greenemeraldagency@gmail.com>
2025-05-02 14:02:42 +0530

    isf

--- src/module/expansive.js
+++ src/module/expansive.js
@@ -1,33 +1,32 @@
 const mongoose = require("mongoose");
 
-const expansiveSchema = new mongoose.Schema(
-  {
-    payeeName: { type: String, required: true },
-    payeePhoneNumber: { type: String, required: true },
-    particulars: { type: String, required: true },
-    // category: { type: String, required: true },
-    amount: { type: Number, required: true },
-    amountInWords: { type: String, required: true },
-    approved: { type: Boolean, default: false },
-    invoiceNo: { type: String,},
-    approvedAmount: {
-      type: Number,
-      default: 0, 
-  },
-
-    chequeDetails: { type: String, default: "" },
-    category: {
-      type: String,
-      
+const expansiveSchema = new mongoose.Schema({
+  payeeName: { type: String, required: true },
+  payeePhoneNumber: { type: String, required: true },
+  particulars: { type: String, required: true },
+  amount: { type: Number, required: true },
+  amountInWords: { type: String, required: true },
+  approved: { type: Boolean, default: false },
+   invoiceNo: {
+        type: String,
+        unique: true,
+        sparse: true // <== THIS IS IMPORTANT
     },
-    paymentMode: { type: String },
-    qrTransactionId: { type: String, default: "" },
-    bankTransferId: { type: String, default: "" },
-    cashDenominations: { type: String, default: "" },
-    AccountentName: { type: String, default: "" },
-  },
   
-);
+  approvedAmount: {
+    type: Number,
+    default: 0,
+  },
+  chequeDetails: { type: String, default: "" },
+  category: { type: String },
+  paymentMode: { type: String },
+  qrTransactionId: { type: String, default: "" },
+  bankTransferId: { type: String, default: "" },
+  cashDenominations: { type: String, default: "" },
+  AccountentName: { type: String, default: "" },
+  billNo: { type: String, default: "" },
+
+});
+
 
 module.exports = mongoose.model("expansive", expansiveSchema);
-    
